4d70e981b9d83c32ce06668ae5c9b84755480704
Barış Mutlu <mutlubaris_1@hotmail.com>
2020-09-13 19:45:42 +0300

    13.09.20 19.45

--- Assets/Scripts/BallMotion.cs
+++ Assets/Scripts/BallMotion.cs
@@ -5,25 +5,25 @@ using UnityEngine.UI;
 
 public class BallMotion : MonoBehaviour
 {
-    [SerializeField] [Range(10, 50)] int _lineSegment = 10;
-    
-    [SerializeField] float _minimumJump = 0.1f;
+    [SerializeField] [Range(10, 50)] int _lineSegment = 10; //Number of vertices for the arc visualization
+
+    [SerializeField] float _minimumJump = 0.05f; //The min jump height of the ball in order to keep bouncing
 
     LineRenderer _lineRenderer;
-    LayerMask _clickMask;
+    LayerMask _clickMask; //The layer in which the Plane resides
+
     JumpSlider _jumpSlider;
     SpeedSlider _speedSlider;
     BounceSlider _bounceSlider;
-
-    Vector3 _startPosition;
-    Vector3 _targetPosition;
-    Vector3 _cursorPosition;
-    Vector3[] _checkpoints;
-
-    int _checkpointIndex = 1;
     float _jumpMultiplier;
     float _speedMultiplier;
     float _bounceMultiplier;
+
+    Vector3 _startPosition;
+    Vector3 _targetPosition;
+
+    Vector3[] _checkpoints;
+    int _checkpointIndex = 1;
 
     bool _movementStarted;
 
@@ -44,30 +44,32 @@ public class BallMotion : MonoBehaviour
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
 
+        //If the ball is ready to launch and the cursor is on the Plane, we can try to calculate the path
         if (Physics.Raycast(ray, out hit, 1000f, _clickMask) && !_movementStarted)
         {
-            _cursorPosition = hit.point;
+            _targetPosition = hit.point;
 
-            if (Input.GetMouseButtonDown(0))
+            if (Input.GetMouseButtonDown(0)) //Start the movement after calculating the path points and disable the visualization
             {
                 _checkpoints = new Vector3[_lineRenderer.positionCount];
                 _lineRenderer.GetPositions(_checkpoints);
                 _lineRenderer.enabled = false;
                 _movementStarted = true;
+
+                _speedMultiplier = _speedSlider.speedValue; //We need to check these values only when the ball is launched
+                _bounceMultiplier = _bounceSlider.bounceValue; //We need to check these values only when the ball is launched
             }
 
-            _jumpMultiplier = _jumpSlider.jumpValue;
-            _speedMultiplier = _speedSlider.speedValue;
-            _bounceMultiplier = _bounceSlider.bounceValue; 
-
-            Vector3 vo = CalculateVelocity(_cursorPosition, _startPosition, 1f);
-            Visualize(vo);
+            _jumpMultiplier = _jumpSlider.jumpValue; //We need to check this value every frame so that the arc is drawn correctly
+            
+            Vector3 vo = CalculateVelocity(_targetPosition, _startPosition, 1f); //Determine the Velocity of the ball
+            Visualize(vo); //Draw the predicted path according to the jump multiplier and the Velocity
         }
 
         if (_movementStarted) MoveBall();
     }
 
-    Vector3 CalculateVelocity(Vector3 target, Vector3 origin, float time)
+    Vector3 CalculateVelocity(Vector3 target, Vector3 origin, float time) //Determines the velocity of the ball
     {
         Vector3 distance = target - origin;
         Vector3 distanceXZ = distance;
@@ -86,7 +88,7 @@ public class BallMotion : MonoBehaviour
         return result;
     }
 
-    Vector3 CalculatePosition(Vector3 vo, float time)
+    Vector3 CalculatePosition(Vector3 vo, float time) //Determines the position of the ball at any given point in time
     {
         Vector3 Vxz = vo;
         Vxz.y = 0f;
@@ -99,7 +101,7 @@ public class BallMotion : MonoBehaviour
         return result;
     }
 
-    void Visualize(Vector3 vo)
+    void Visualize(Vector3 vo) //Updates the linerenderer according to the calculated positions
     {
         _lineRenderer.positionCount = _lineSegment + 1;
         
@@ -109,35 +111,35 @@ public class BallMotion : MonoBehaviour
             _lineRenderer.SetPosition(i, pos); 
         }
 
-        _lineRenderer.SetPosition(_lineSegment, _cursorPosition);
+        _lineRenderer.SetPosition(_lineSegment, _targetPosition);
     }
 
     void MoveBall()
     {
-        if (_checkpointIndex < _checkpoints.Length)
+        if (_checkpointIndex < _checkpoints.Length) //If we haven't reached the ground yet
         {
             transform.position = Vector3.MoveTowards(transform.position, _checkpoints[_checkpointIndex], _speedMultiplier * Time.deltaTime * 5);
 
-            if (transform.position == _checkpoints[_checkpointIndex]) 
+            if (transform.position == _checkpoints[_checkpointIndex]) //Start moving to the next path point
             {
                 _checkpointIndex++;
             }
         }
-        else
+        else //When we reach the ground, we need to calculate the new path points according to the bounciness
         {
-            for (int i = 0; i < _checkpoints.Length; i++)
+            for (int i = 0; i < _checkpoints.Length; i++) //Select every path point
             {
-                _checkpoints[i] += (_cursorPosition - _startPosition);
-                _checkpoints[i] -= ((_checkpoints[i] - transform.position) * (1 - (_bounceMultiplier / 10)));
+                _checkpoints[i] += (_targetPosition - _startPosition); //Carry the path points to the current spot
+                _checkpoints[i] -= ((_checkpoints[i] - transform.position) * (1 - (_bounceMultiplier / 10))); //Subtract the bounce loss
             }
-            _cursorPosition = _checkpoints[(_checkpoints.Length - 1)];
-            _startPosition = transform.position;
+            _targetPosition = _checkpoints[(_checkpoints.Length - 1)]; //Update the target point for the next time we are going to carry the path points
+            _startPosition = transform.position; //Update the start point for the next time we are going to carry the path points
 
 
             _jumpMultiplier *= (_bounceMultiplier / 10);
 
-            if (_jumpMultiplier > _minimumJump) _checkpointIndex = 1;
-            else Destroy(gameObject);
+            if (_jumpMultiplier > _minimumJump) _checkpointIndex = 1; //Check if the next bounce is going to be high enough
+            else Destroy(gameObject); //Otherwise destroy the gameobject
         }
     }
 }
--- Assets/Scripts/BallSpawner.cs
+++ Assets/Scripts/BallSpawner.cs
@@ -19,22 +19,28 @@ public class BallSpawner : MonoBehaviour
 
     private void Update() 
     {
-        _ballCounter.text = (25 - gameObject.transform.childCount).ToString(); //Update ball count on UI
-        
+        _ballCounter.text = (25 - gameObject.transform.childCount).ToString(); //Update ball counter on UI
+        //The number of balls will increase whenever a ball is destroyed, because the childcount will decrease
+
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
 
-        if (Physics.Raycast(ray, out hit, 1000f, _clickMask))
+        if (Physics.Raycast(ray, out hit, 1000f, _clickMask)) //If the cursor is on the Plane
         {
-            _target.transform.position = hit.point + Vector3.up * 0.01f;
-            if (Input.GetMouseButtonDown(0) && gameObject.transform.childCount < 24)
+            _target.transform.position = hit.point + Vector3.up * 0.01f; //Update the cursor highlighter's position
+            if (Input.GetMouseButtonDown(0) && gameObject.transform.childCount < 24) //If we click and still have balls in the pool
             {
-                if(!_readyForLaunch)
+                if(!_readyForLaunch) //Instantiate a ball if there isn't one ready to go
+                
                 {
                     var ball = Instantiate(_ball, hit.point, Quaternion.identity);
-                    ball.transform.parent = gameObject.transform;
+                    ball.transform.parent = gameObject.transform; //We will keep track of the number of balls via the childcount
                 }
-                _readyForLaunch = !_readyForLaunch;
+
+                //If we already have a ball ready to launch, the click will launch the current ball via the BallMotion script
+                //Therefore we only need to switch the bool so that we are ready to Instantiate a new ball
+
+                _readyForLaunch = !_readyForLaunch;  
             }
         }
     }
